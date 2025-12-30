acd8e548ba6e9498a74c715a6979ae3c960f7b4d
ankanZtoH <ankan06edu@gmail.com>
2025-12-31 00:42:20 +0530

    edited

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
 
   // basePath: "/teams",
+  // assetPrefix: "/~perceptron/teams",
+
+  // trailingSlash: true,
 
   images: {
     unoptimized: true,
